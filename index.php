f72f6db6656f72753fed7b3a326123d853105ee6
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-12-19 19:00:52 -0300

    32. 42. Personalizando Tailwind al Proyecto

--- index.php
+++ index.php
@@ -5,11 +5,17 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Tailwind Animated Portfolio</title>
 	<link rel="stylesheet" type="text/css" href="src/css/estilos.css">
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,300;9..40,400;9..40,500;9..40,700&family=Montserrat:wght@300;400;700&family=Mulish:wght@300;400;600&family=Poppins:wght@200;400&family=Roboto:wght@300;400&display=swap" rel="stylesheet">
 </head>
 
-<body>
+<body class="font-dm-sans bg-color-background text-white">
 
-	<h1>Animated Portfolio</h1>
+	<div class="w-[90%] mx-auto overflow-hidden max-w-screen-xl">
+		<h1>Tailwind Portfolio</h1>
+	</div>
 	
 </body>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./index.php", "./landing.php"],
+  content: ["./src/**/*.{html,js}", "./index.php"],
   theme: {
-    extend: {},
+    extend: {
+
+      fontFamily: {
+        "dm-sans": "font-family: 'DM Sans', sans-serif", 
+      },
+
+      colors:{
+        "green-color": "#27ae60",
+        "gray-color": "#828282",
+        "card-color": "#212121",
+        "color-background": "#171717",
+        "menu-color": "#172554"
+      },
+
+      backgroundImage: {
+        "open-menu": "url('img/menu.svg')",
+        "open-menu": "url('img/close.svg')"
+      }
+
+    },
   },
   plugins: [],
 }
-
