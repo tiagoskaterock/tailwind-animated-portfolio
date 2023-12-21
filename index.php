5e5d37d3c81922d16fc67503e7ca53d3883bfe41
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-12-20 22:45:09 -0300

    44. Terminando la sección principal del proyecto

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 		
 		<header>
 			
-			<nav class="flex h-20 bg-red-800 items-center justify-between">
+			<nav class="flex h-20 md:bg-red-800 items-center justify-between">
 
 				<a href="#" class="w-1/2 max-w-[200px]">
 					<img 
@@ -33,19 +33,65 @@
 
 				<label 
 					for="menu" 
-					class="text-white w-10 h-10 bg-open-menu cursor-pointer bg-cover bg-center bg-green-color rounded-lg text-white peer-checked/menu:bg-close-menu transition-all z-40">
+					class="text-white w-10 h-10 bg-open-menu cursor-pointer bg-cover bg-center bg-green-color rounded-lg text-white peer-checked/menu:bg-close-menu transition-all z-40 md:hidden">
 				</label>
 
-				<ul class="fixed inset-0 bg-menu-color px-[5%] grid auto-rows-max content-center justify-items-center clip-circle-0 peer-checked/menu:clip-circle-full">
+				<ul class="fixed inset-0 bg-menu-color px-[5%] grid gap-6 auto-rows-max content-center justify-items-center clip-circle-0 peer-checked/menu:clip-circle-full transition-[clip-path] duration-500 md:clip-circle-full md:relative md:grid-flow-col md:py-0 md:bg-transparent">
 					<li>
 						<a href="#">Home</a>
+					</li>
+
+					<li>
 						<a href="#">About</a>
+					</li>
+
+					<li>
 						<a href="#">Services</a>
+					</li>
+
+					<li>
 						<a href="#">Contact</a>
 					</li>
 				</ul>
 
 			</nav>
+
+
+
+			<section 
+				class="pt-10 pb-16 md:flex justify-between items-center gap-8">
+				<figure class="md:order-1">
+					<img 
+						class="w-[90%] aspect-square object-cover rounded-full max-w-sm mx-auto"
+						src="girl.jpg" 
+						alt="Background Image">
+				</figure>
+
+				<article class="text-center mt-8 md:w-1/2 md:text-left">
+
+					<h1 class="text-4xl">Hello! I am Tiago Lemes.</h1>
+
+					<p class="text-gray-color text-xl mt-3 mb-6">Web and Game Developer</p>
+
+					<div class="grid grid-cols-[repeat(auto-fit,minmax(170px,1fr))] auto-rows-[60px] gap-8 max-w-lg mx-auto md:m-0">
+
+						<a 
+							href="#" 
+							class="bg-green-color flex items-center justify-center">
+							Download CV
+						</a>
+
+						<a 
+							href="#"
+							class="bg-transparent flex items-center justify-center border-white border">
+							Learn More
+						</a>
+
+					</div>
+				</article>
+
+			</section>
+
 
 		</header>
 
